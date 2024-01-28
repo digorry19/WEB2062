46a214d28b142ae1454c320cf3e9df1b768a6e64
digorry19 <125790557+digorry19@users.noreply.github.com>
2024-01-28 23:50:38 +0700

    Add files via upload

---
+++ Lab 3/lab3.2.js
@@ -0,0 +1,20 @@
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  // dogsJulia.slice(1, 3);
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  console.log(dogs);
+
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(
+        `Con chó số ${i + 1} là con chó trưởng thành, và ${dog} tuổi`
+      );
+    } else {
+      console.log(`Con chó số ${i + 1} vẫn là chó con 🐶`);
+    }
+  });
+};
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
